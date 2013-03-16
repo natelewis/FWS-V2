375f40e3766ef4883ffa193f8f9df670f8fcd818
Nate Lewis <nlewis@gnetworks.com>
2013-03-16 17:43:51 -0400

    hash tick cleanup

--- lib/FWS/V2/Check.pm
+++ lib/FWS/V2/Check.pm
@@ -30,7 +30,7 @@ our $VERSION = '0.003';
 	#
 	# all simple boolean response in conditionals
 	#
-	if ($fws->isValidEmail('this@email.com')) { print "Its not real, but it could be!\n" } 
+	if ( $fws->isValidEmail( 'this@email.com') ) { print "Its not real, but it could be!\n" } 
 	else { print "Yuck, bad email.\n" } 
 
 
@@ -51,14 +51,15 @@ Return a 0 or 1 depending if a admin user is currently logged in.
         #
         # do something if logged in as an admin user
         #
-        if ($fws->isAdminLoggedIn()) { $valueHash{'html'} .= 'I am logged in as a admin<br/>' }
+        if ( $fws->isAdminLoggedIn() ) { $valueHash{html} .= 'I am logged in as a admin<br/>' }
 
 =cut
 
 sub isAdminLoggedIn {
-        my ($self,$loginType) = @_;
-        if ($self->{'adminLoginId'} ne '') { return (1) } else { return (0) }
-        }
+    my ( $self, $loginType ) = @_;
+    if ( $self->{adminLoginId} ) { return 1 }
+    return 0;
+}
 
 =head2 isUserLoggedIn
 
@@ -67,13 +68,14 @@ Return a 0 or 1 depending if a site user is currently logged in.
         #
         # do something if logged in as an site user
         #
-        if ($fws->isUserLoggedIn()) { $valueHash{'html'} .= 'I am logged in as a user<br/>' }
+        if ( $fws->isUserLoggedIn() ) { $valueHash{html} .= 'I am logged in as a user<br/>' }
 
 =cut
 
 sub isUserLoggedIn {
-        my ($self,$loginType) = @_;
-        if ($self->{'userLoginId'} eq '') { return (0) } else { return (1) }
+    my ( $self, $loginType ) = @_;
+    if ( $self->{userLoginId} ) { return 1 }
+    return 0;
 }
 
 =head2 isValidEmail
@@ -83,9 +85,9 @@ Return a boolean response to validate if an email address is well formed.
 =cut
 
 sub isValidEmail {
-        my ($self,$fieldValue) = @_;
-        if ($fieldValue !~ /^\w+[\w|\.|-]*\w+@(\w+[\w|\.|-]*\w+\.[a-z]{2,4}|(\d{1,3}\.){3}\d{1,3})$/i) { return (0) }
-        return (1);
+    my ( $self, $fieldValue ) = @_;
+    if ( $fieldValue !~ /^\w+[\w|\.|-]*\w+@(\w+[\w|\.|-]*\w+\.[a-z]{2,4}|(\d{1,3}\.){3}\d{1,3})$/i ) { return 0 }
+    return 1;
 }
 
 =head2 isCaptchaValid
@@ -95,18 +97,18 @@ Built in captcha support will return 1 or 0 based on the last captcha post.
 =cut
 
 sub isCaptchaValid {
-        my ($self) = @_;
-        my $publicKey = $self->siteValue("captchaPublicKey");
-        my $privateKey = $self->siteValue("captchaPrivateKey");
-        my $returnHTML;
-        if ($publicKey ne "") {
-                require Captcha::reCAPTCHA;
-                Captcha::reCAPTCHA->import();
-                my $captcha = Captcha::reCAPTCHA->new();
-                my $result = $captcha->check_answer($privateKey, $ENV{"REMOTE_ADDR"}, $self->formValue("recaptcha_challenge_field"), $self->formValue("recaptcha_response_field"));
-                if ( $result->{"is_valid"} ) { return "1" } else { return "0" }
-        }
-        return 1;
+    my ( $self ) = @_;
+    my $publicKey   = $self->siteValue( 'captchaPublicKey' );
+    my $privateKey  = $self->siteValue( 'captchaPrivateKey' );
+    my $returnHTML;
+    if ( $publicKey ) {
+        require Captcha::reCAPTCHA;
+        Captcha::reCAPTCHA->import();
+        my $captcha = Captcha::reCAPTCHA->new();
+        my $result = $captcha->check_answer( $privateKey, $ENV{REMOTE_ADDR}, $self->formValue( 'recaptcha_challenge_field' ), $self->formValue( 'recaptcha_response_field' ) );
+        if ( !$result->{is_valid} ) { return 0 }
+    }
+    return 1;
 }
 
 
@@ -117,9 +119,9 @@ FWS standard strong password checker.   Upper, lower, symbol, at least 6 chars.
 =cut
 
 sub isStrongPassword {
-        my ($self,$fieldValue) = @_;
-        if ($fieldValue !~ /^.*(?=.{6,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$/) { return 0 }
-        return 1;
+    my ( $self, $fieldValue ) = @_;
+    if ( $fieldValue !~ /^.*(?=.{6,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$/) { return 0 }
+    return 1;
 }
 
 =head2 isElementPresent
@@ -129,39 +131,39 @@ See if an element is present on the current page.  This is here for some legacy 
 =cut
 
 sub isElementPresent {
-        my ($self,$guid,$elementName) = @_;
+    my ( $self, $guid, $elementName ) = @_;
 
-        #
-        # Lets check if the formavalue FWS_elementblahblah is set if, so we have already looked this up and don't need to re-run it
-        #
-        my $isPresent = $self->formValue("FWS_ELEMENT_PRESENT_".$elementName);
+    #
+    # Lets check if the formavalue FWS_elementblahblah is set if, so we have already looked this up and don't need to re-run it
+    #
+    my $isPresent = $self->formValue( 'FWS_ELEMENT_PRESENT_' . $elementName );
 
-        #
-        # if it is blank, then we do need to run it for the first time :(
-        #
-        if ($isPresent eq '') {
+    #
+    # if it is blank, then we do need to run it for the first time :(
+    #
+    if ( !$isPresent ) {
 
-                #
-                # pull from the database to see if its there
-                #
-                my $pageId = $self->getPageGUID($guid);
-                ($isPresent) = $self->openRS("select 1 from data left join guid_xref on data.guid=child where guid_xref.parent='".$pageId."' and data.site_guid='".$self->safeSQL($self->{'siteGUID'})."' and (element_type like '".$self->safeSQL($elementName)."')");
+            #
+            # pull from the database to see if its there
+            #
+            my $pageId = $self->getPageGUID( $guid );
+            ( $isPresent ) = @{$self->runSQL( SQL => "select 1 from data left join guid_xref on data.guid=child where guid_xref.parent='". $self->safeSQL( $pageId ) . "' and data.site_guid='" . $self->safeSQL( $self->{siteGUID} ) . "' and (element_type like '" . $self->safeSQL( $elementName ) . "')" )};
 
-                #
-                # if it comes back as "NO NO NO!"  then it will be blank.  so we will need to set it to 0
-                #
-                if ($isPresent eq '') { $isPresent = 0 }
+            #
+            # if it comes back as "NO NO NO!"  then it will be blank.  so we will need to set it to 0
+            #
+            $isPresent ||= 0;
 
-                #
-                # Set the form value to what the value is so then we don't have to worry about it the next time we are here
-                #
-                $self->formValue("FWS_ELEMENT_PRESENT_".$elementName,$isPresent);
-        }
+            #
+            # Set the form value to what the value is so then we don't have to worry about it the next time we are here
+            #
+            $self->formValue( 'FWS_ELEMENT_PRESENT_' . $elementName, $isPresent );
+    }
 
-        #
-        # pass back the value if we have gotten it from the cache or we had to look it up
-        #
-        return $isPresent;
+    #
+    # pass back the value if we have gotten it from the cache or we had to look it up
+    #
+    return $isPresent;
 }
 
 
